bb9a7dcd9c40daf8431aa3a014e18557431ac116
Jonas Oliveira <jno@outllok.pt>
2022-11-17 13:57:38 +0000

    feature(#19): created user_id foreign key column in Event table, added relation in event model with belongTo and hasMany in User model, added auth relation with the user in controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -53,6 +53,9 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+        
         $event->save();
 
         return redirect('/');
@@ -64,6 +67,7 @@ class EventController extends Controller
 
         return view('events.show', ['event' => $event]);
     }
+
 
 }
 
