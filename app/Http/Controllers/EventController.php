25cb4fb07c62718de6f9cf7d900cfba969226473
jonas <jnoliveira@pdmfc.com>
2022-11-21 23:30:24 +0000

    feature(#24): add destroy function in controller, added delete form and adjusted dashboard style

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -80,5 +80,11 @@ class EventController extends Controller
         return view('events.dashboard',['events' => $events]);
     }
 
+    public function destroy($id)
+    {
+        Event::findOrFail($id)->delete();
+
+        return redirect('dashboard')->with('msg', 'Successfully deleted event!');
+    }
 }
 
--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
 
-    <div class="col-md-10 offset-md-1 deshboard-title-container">
+    <div class="col-md-10 offset-md-1 dashboard-title-container">
         <h1>My events</h1>
     </div>
 
-    <div class="col-md-10 offset-md-1 deshboard-events-container">
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
         @if (count($events) > 0)
             <table class="table">
                 <thead>
